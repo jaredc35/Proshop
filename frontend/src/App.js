5a00e3e98f1b353e99273dc31e6e9f079e0ec9a2
Jared <jaredt.conway@gmail.com>
2021-01-06 10:23:40 -0500

    Add rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
 const App = () => {
   return (
@@ -7,7 +8,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,10 +9,10 @@ const Header = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <Nav.Link href="/cart">
-              <i className="fas fa-shopping-cart"></i>Cart
+              <i className="fas fa-shopping-cart"></i> Cart
             </Nav.Link>
             <Nav.Link href="/login">
-              <i className="fas fa-user"></i>Sign In
+              <i className="fas fa-user"></i> Sign In
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
