7b67d831eabc78f7a415562b4ec6ea80eb1ded8e
Jared <jaredt.conway@gmail.com>
2021-01-12 17:54:01 -0500

    Add put route for users to edit their profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,12 +4,14 @@ import { protect } from "../middleware/authMiddleware.js";
 import {
   authUser,
   registerUser,
-  getUserProfile
+  getUserProfile,
+  updateUserProfile
 } from "../controllers/userController.js";
 
 router.post("/login", authUser); // Get a token for user
 
 router.post("/", registerUser); // Register a new user
 router.route("/profile").get(protect, getUserProfile); // Get the user profile info
+router.route("/profile").put(protect, updateUserProfile);
 
 export default router;
